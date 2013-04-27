baaf41a426afefd659fa855c8ea61f5c4b8465ef
samvrai <t3echelon@gmail.com>
2013-04-27 23:03:12 +0200

    modificada interfaz de ruben: bannerloginerror

--- src/tringesInterfaces/InterfazBannerErrorLogin.java
+++ src/tringesInterfaces/InterfazBannerErrorLogin.java
@@ -34,6 +34,11 @@ public class InterfazBannerErrorLogin extends javax.swing.JFrame {
         jLabel1.setText("El usuario o la contraseña introducidas son erróneas.");
 
         jButton1.setText("Aceptar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -61,6 +66,12 @@ public class InterfazBannerErrorLogin extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+        new InterfazIndentificarse().setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
